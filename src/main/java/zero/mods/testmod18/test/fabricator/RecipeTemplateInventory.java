15fca255e5771b5a43a34f48fb0fc2d7c053e407
ZeroNoRyouki <marco@adachi.it>
2015-07-07 01:26:14 +0200

    Initial support for GUIs and code cleanup

---
+++ src/main/java/zero/mods/testmod18/test/fabricator/RecipeTemplateInventory.java
@@ -0,0 +1,111 @@
+package zero.mods.testmod18.test.fabricator;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.IChatComponent;
+
+public class RecipeTemplateInventory implements IInventory {
+
+
+    public RecipeTemplateInventory() {
+
+        this._recipeItemStacks = new ItemStack[9];
+    }
+
+
+    @Override
+    public int getSizeInventory() {
+
+        return this._recipeItemStacks.length;
+    }
+
+    @Override
+    public ItemStack getStackInSlot(int index) {
+        return null;
+    }
+
+    @Override
+    public ItemStack decrStackSize(int index, int count) {
+        return null;
+    }
+
+    @Override
+    public ItemStack getStackInSlotOnClosing(int index) {
+        return null;
+    }
+
+    @Override
+    public void setInventorySlotContents(int index, ItemStack stack) {
+
+    }
+
+    @Override
+    public int getInventoryStackLimit() {
+        return 0;
+    }
+
+    @Override
+    public void markDirty() {
+
+    }
+
+    @Override
+    public boolean isUseableByPlayer(EntityPlayer player) {
+        return false;
+    }
+
+    @Override
+    public void openInventory(EntityPlayer player) {
+
+    }
+
+    @Override
+    public void closeInventory(EntityPlayer player) {
+
+    }
+
+    @Override
+    public boolean isItemValidForSlot(int index, ItemStack stack) {
+        return false;
+    }
+
+    @Override
+    public int getField(int id) {
+        return 0;
+    }
+
+    @Override
+    public void setField(int id, int value) {
+
+    }
+
+    @Override
+    public int getFieldCount() {
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public boolean hasCustomName() {
+        return false;
+    }
+
+    @Override
+    public IChatComponent getDisplayName() {
+        return null;
+    }
+
+
+
+    private final ItemStack[] _recipeItemStacks;
+}
