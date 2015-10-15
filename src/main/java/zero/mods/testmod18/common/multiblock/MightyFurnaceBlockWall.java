6860bd35bd60be6d2e29677802d098a6c0439e3e
ZeroNoRyouki <marco@adachi.it>
2015-10-15 16:58:07 +0200

    migrated blockstate handling to PropertyBlockFacings

--- src/main/java/zero/mods/testmod18/common/multiblock/MightyFurnaceBlockWall.java
+++ src/main/java/zero/mods/testmod18/common/multiblock/MightyFurnaceBlockWall.java
@@ -1,6 +1,8 @@
 package zero.mods.testmod18.common.multiblock;
 
+import com.google.common.collect.Lists;
 import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.BlockState;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.ItemStack;
@@ -8,8 +10,10 @@ import net.minecraft.util.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import zero.mods.testmod18.common.multiblock.tile.MightyFurnaceTileEntity;
 import zero.mods.zerocore.common.lib.BlockFacings;
+import zero.mods.zerocore.common.lib.PropertyBlockFacings;
 import zero.mods.zerocore.common.multiblock.IMultiblockPart;
 
+import java.util.ArrayList;
 
 
 public class MightyFurnaceBlockWall extends MightyFurnaceBlockBase {
@@ -41,7 +45,8 @@ public class MightyFurnaceBlockWall extends MightyFurnaceBlockBase {
             boolean assembled = wallTile.isConnected() && wallTile.getMultiblockController().isAssembled();
             BlockFacings facings = assembled ? wallTile.getOutwardsDir() : BlockFacings.ALL;
 
-            return facings.toBlockState(state);
+            //return facings.toBlockState(state);
+            state = state.withProperty(FACES, facings.toProperty());
         }
 
         return state;
@@ -50,15 +55,30 @@ public class MightyFurnaceBlockWall extends MightyFurnaceBlockBase {
     @Override
     protected BlockState createBlockState() {
 
-        return new BlockState(this, new IProperty[] {BlockFacings.FACING_DOWN, BlockFacings.FACING_UP, BlockFacings.FACING_WEST,
-                BlockFacings.FACING_EAST, BlockFacings.FACING_NORTH, BlockFacings.FACING_SOUTH});
+        return new BlockState(this, new IProperty[] {FACES});
     }
 
     @Override
     protected void initBlock() {
 
         super.initBlock();
-        this.setDefaultState(BlockFacings.ALL.toBlockState(this.blockState.getBaseState()));
+        //this.setDefaultState(BlockFacings.ALL.toBlockState(this.blockState.getBaseState()));
+        this.setDefaultState(this.blockState.getBaseState().withProperty(FACES, PropertyBlockFacings.All));
     }
+
+    private final static PropertyEnum FACES ;
+
+    static {
+
+        ArrayList<PropertyBlockFacings> values = Lists.newArrayList();
+
+        values.addAll(PropertyBlockFacings.ALL_AND_NONE);
+        values.addAll(PropertyBlockFacings.FACES);
+        values.addAll(PropertyBlockFacings.ANGLES);
+        values.addAll(PropertyBlockFacings.CORNERS);
+
+        FACES = PropertyEnum.create("faces", PropertyBlockFacings.class, values);
+    }
+
 
 }
